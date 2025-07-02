c2ee41f1abd5ba384e4d2cd1a8a79a10f5037ecc
Kritika Panda <128455821+kritika-panda@users.noreply.github.com>
2025-07-02 15:54:00 +0530

    Create DebugLogProcessor.java

---
+++ Design Patterns/Chain Of Responsibility/DebugLogProcessor.java
@@ -0,0 +1,13 @@
+package com.debugprocessor;
+public class DebugLogProcessor extends LogProcessor {
+    public DebugLogProcessor(LogProcessor nextLogProcessor) {
+        super(nextLogProcessor);
+    }
+    public void log(int logLevel, String message) {
+        if (logLevel == DEBUG) {
+            System.out.println("DEBUG: " + message);
+        } else {
+            super.log(logLevel, message);
+        }
+    }
+}

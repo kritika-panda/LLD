34df212d7cccad5c7e2ca050f3eff9465326c56d
Kritika Panda <128455821+kritika-panda@users.noreply.github.com>
2025-07-03 20:45:21 +0530

    Create BeverageMaker.java

---
+++ Design Patterns/Template Design Pattern/BeverageMaker.java
@@ -0,0 +1,21 @@
+abstract class BeverageMaker {
+    // Template method
+    public final void makeBeverage() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+}
+

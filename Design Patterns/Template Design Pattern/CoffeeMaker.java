d39c1fd8636e42aed5ff2493a9306a2fb2eee846
Kritika Panda <128455821+kritika-panda@users.noreply.github.com>
2025-07-03 20:45:52 +0530

    Create CoffeeMaker.java

---
+++ Design Patterns/Template Design Pattern/CoffeeMaker.java
@@ -0,0 +1,9 @@
+class CoffeeMaker extends BeverageMaker {
+    void brew() {
+        System.out.println("Dripping coffee through filter");
+    }
+
+    void addCondiments() {
+        System.out.println("Adding sugar and milk");
+    }
+}

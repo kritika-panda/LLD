a80345c44dda2cb05c8d51c758c57690a71a7d57
Kritika Panda <128455821+kritika-panda@users.noreply.github.com>
2025-07-03 20:45:38 +0530

    Create TeaMaker.java

---
+++ Design Patterns/Template Design Pattern/TeaMaker.java
@@ -0,0 +1,10 @@
+class TeaMaker extends BeverageMaker {
+    void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    void addCondiments() {
+        System.out.println("Adding lemon");
+    }
+}
+
